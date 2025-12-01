cba844e01ac464aa39eb6645bb7a8be8c398abd4
David Širůček <davidsirucek@seznam.cz>
2025-12-01 21:01:53 +0100

    Constants : přidali jsme konstanty time a date_formatter

--- app/src/main/java/Cinema/Constants.java
+++ app/src/main/java/Cinema/Constants.java
@@ -8,5 +8,10 @@ public class Constants {
 	public static final String DATE_TIME_FORMAT = "dd.MM.yyyy HH:mm";
 	public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT);
 	
+	public static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
+	public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 	
+	public static final String SEAT_SEPARATOR = "x"; //TODO použít regulární výraz ve formátu řada x sedadlo
+	
+
 }
